b265b17a0f5fbdc063661a24ccff5accd8dc7f3a
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-11-26 11:15:26 +0100

    Authentification tokenisé avec Laravel Token

---
+++ app/Http/Controllers/Api/AuthControllerplaravel/airlock.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\AuthControllerplaravel;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class airlock extends Controller
+{
+    //
+}
