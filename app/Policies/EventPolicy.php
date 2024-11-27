b4462a96a3d015ea916e2f15459e51fcdf6cf4d7
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-11-27 14:52:14 +0100

    Policies avec toutes les autorisations marchant perfectement

--- app/Policies/EventPolicy.php
+++ app/Policies/EventPolicy.php
@@ -11,60 +11,51 @@ class EventPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(?User $user): bool
     {
-        //
         return true;
     }
-
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Event $event): bool
+    public function view(?User $user, Event $event): bool
     {
-        //
         return true;
     }
-
     /**
      * Determine whether the user can create models.
      */
     public function create(User $user): bool
     {
-        //
         return true;
     }
-
     /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Event $event): bool
     {
-        //
         return $user->id === $event->user_id;
     }
-
     /**
-     * Determine whether the user can delete the model.
+     * Determine whether the user can delete the
+     * /
+
+     * Determine whether the user can restore the model.
      */
     public function delete(User $user, Event $event): bool
     {
-        //
         return $user->id === $event->user_id;
     }
-
     /**
      * Determine whether the user can restore the model.
      */
     // public function restore(User $user, Event $event): bool
     // {
     //     //
-
     // }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
+    // /**
+    //  * Determine whether the user can permanently delete the model.
+    //  */
     // public function forceDelete(User $user, Event $event): bool
     // {
     //     //
