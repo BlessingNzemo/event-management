0a8b2a855f529c0c0504bc0d08b0feffc578ad09
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-11-29 16:57:59 +0100

    Queues in Laravel

--- app/Notifications/EventReminderNotification.php
+++ app/Notifications/EventReminderNotification.php
@@ -7,7 +7,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use App\Models\Event;
-class EventReminderNotification extends Notification
+class EventReminderNotification extends Notification implements ShouldQueue
+
 {
     use Queueable;
 
