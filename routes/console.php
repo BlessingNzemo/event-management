e2a36b90bf407c11ff99ab9dfd7cf160641b12a1
Blessing Nzemo <blessingnzemojph@gmail.com>
2024-11-29 16:46:13 +0100

    In late

--- routes/console.php
+++ routes/console.php
@@ -2,15 +2,25 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-// Define the schedule within a closure
-Artisan::command('app:send-event-reminders', function (Schedule $schedule) {
-    $schedule->command('app:send-event-reminders')->everyMinute();
-})->purpose('Schedule event reminders every fifteen minutes');
+
+Artisan::command('app:send-event-reminders', function () {
+
+    $events = \App\Models\Event::with('attendees.user')
+            ->whereBetween('start_time', [now(), now()->addDay()])
+            ->get();
+        $eventCount = $events->count();
+        $eventLabel = \Illuminate\Support\Str::plural('event', $eventCount);
+        $this->info("Found {$eventCount} {$eventLabel}.");
+        $events->each(
+            fn($event) => $event->attendees->each(
+                fn($attendee) => $this->info("Notifie l'utilisateur {$attendee->user->id}")
+            )
+        );
+        $this->info('Les rappels de notification sont envoyés avec succès!');
+
+})->purpose('Affiche les notifications des mails')->everyMinute();
